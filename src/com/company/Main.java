9ccdd56c93a96bf5171b4180b9aa1ff2c3d050a7
greedman231 <93518502+greedman231@users.noreply.github.com>
2021-11-03 13:45:45 +0300

    1

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double cathetus1, cathetus2, hypot;
+
+    cathetus1 = 3;// длина первого катета
+    cathetus2 = 4;// длина второго катета
+
+    hypot = Math.sqrt((cathetus1 * cathetus1) + (cathetus2 * cathetus2));
+    System.out.println("длинна гипотенузы равна " + hypot);
+    }
+}
